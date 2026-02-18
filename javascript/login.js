2f533e62d3d82f1d1d281499d8e9031e115bd47f
onexdhar2 <onexdhar667@gmail.com>
2026-02-18 20:19:02 +0530

    Login page

--- javascript/login.js
+++ javascript/login.js
@@ -1,1 +1,14 @@
-console.log("login functionality connect")
+document.getElementById("login-button").addEventListener("click", () => {
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+  const pinInput = document.getElementById("input-pin");
+  const pinNumber = pinInput.value;
+  console.log(pinNumber);
+  if (contactNumber == "01234567890" && pinNumber == "1234") {
+    // alert('login success')
+    window.location.assign("/homepage.html");
+  } else {
+    // alert ("login failed")
+  }
+});
